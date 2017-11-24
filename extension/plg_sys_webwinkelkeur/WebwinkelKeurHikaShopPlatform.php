15db9459e76cbdbcb106998103b796dc79d0336c
Milko Kosturkov <milko@kiboit.com>
2017-11-24 18:04:13 +0100

    Add support for new API for HikaShop

---
+++ extension/plg_sys_webwinkelkeur/WebwinkelKeurHikaShopPlatform.php
@@ -0,0 +1,164 @@
+<?php
+
+require_once dirname(__FILE__) . '/WebwinkelKeurShopPlatform.php';
+class WebwinkelKeurHikaShopPlatform implements WebwinkelKeurShopPlatform {
+
+    /**
+     * @var JDatabaseDriver
+     * @since 1.1.0
+     */
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getExtensionName() {
+        return 'com_hikashop';
+    }
+
+    public function getPlatformAbbreviation() {
+        return 'hs';
+    }
+
+    public function getClientName() {
+        return 'hikashop';
+    }
+
+    public function getOrdersToInvite() {
+        return $this->db->setQuery("
+            SELECT
+                ho.order_id,
+                ho.order_number,
+                hu.user_email,
+                CONCAT(ha.address_firstname, ' ', ha.address_lastname) as customer_name
+            FROM `#__hikashop_order` ho
+            INNER JOIN `#__hikashop_user` hu ON
+                ho.order_user_id = hu.user_id
+            LEFT JOIN `#__hikashop_address` ha ON
+                ho.order_billing_address_id = ha.address_id
+            LEFT JOIN `#__hikashop_zone` hz ON
+                ha.address_country = hz.zone_namekey
+            LEFT JOIN `#__webwinkelkeur_hikashop_order` who ON
+                who.hikashop_order_id = ho.order_id
+            WHERE
+                (
+                    who.hikashop_order_id IS NULL
+                    OR (
+                        who.success = 0
+                        AND who.tries <= 5
+                        AND who.time < " . (time() - 1800) . "
+                    )
+                )
+                AND hu.user_email LIKE '%@%'
+                AND ho.order_status = 'shipped'
+        ")->loadAssocList();
+    }
+
+    public function getOrderId($order) {
+        return $order['order_number'];
+    }
+
+    public function getOrderEmail($order) {
+        return $order['user_email'];
+    }
+
+    public function getOrderCustomerName($order) {
+        return $order['customer_name'];
+    }
+
+    public function getOrderLanguage($order) {}
+
+    public function getOrderPhones($order) {
+        $order_data = $this->getOrderData($order);
+        $phones = array();
+        foreach (array('invoice_address', 'delivery_address') as $address) {
+            if (isset ($order_data[$address])) {
+                $phones[] = $order_data[$address]['address_telephone'];
+                $phones[] = $order_data[$address]['address_telephone2'];
+            }
+        }
+        return array_unique(array_filter($phones));
+    }
+
+    public function getOrderData($order) {
+        static $orders_cache = array();
+        if (isset ($orders_cache[$order['order_id']])) {
+            return $orders_cache[$order['order_id']];
+        }
+
+        $order_query = "SELECT * FROM `#__hikashop_order` WHERE `order_id` = " . $order['order_id'];
+        $order_info = $this->db->setQuery($order_query)->loadAssoc();
+
+        $order_lines_query = "SELECT * FROM `#__hikashop_order_product` WHERE `order_id` = " . $order['order_id'];
+        $order_info['order_lines'] = array();
+        $products = array();
+        foreach ($this->db->setQuery($order_lines_query)->loadAssocList() as $line) {
+            $order_info['order_lines'][] = $line;
+            $products[] = $this->getProduct($line['product_id']);
+        }
+
+        $customer_query  = "
+            SELECT * 
+            FROM `#__hikashop_user` AS `user`
+            LEFT JOIN `#__users` AS `cms_users`
+              ON `cms_users`.`id` = `user`.`user_cms_id` 
+            LEFT JOIN `#__hikashop_currency` AS `currency`
+              ON `currency`.`currency_id` = `user`.`user_currency_id`
+            WHERE `user`.`user_id` = " . $order_info['order_user_id'];
+        $customer = $this->db->setQuery($customer_query)->loadAssoc();
+        unset ($customer['password']);
+
+        $order_data = array(
+            'order' => $order_info,
+            'products' => $products,
+            'customer' => $customer,
+            'delivery_address' => array(),
+            'invoice_address' => array()
+        );
+
+        $address_ids = array_filter(array(
+            $order_info['order_billing_address_id'],
+            $order_info['order_shipping_address_id']
+        ));
+        if (!empty ($address_ids)) {
+            $address_query =
+                "SELECT * FROM `#__hikashop_address` WHERE `address_id` IN (" . join(',', $address_ids) . ")";
+            foreach ($this->db->setQuery($address_query)->loadAssocList() as $address) {
+                if ($address['address_id'] == $order_info['order_billing_address_id']) {
+                    $order_data['invoice_address'] = $address;
+                } else {
+                    $order_data['delivery_address'] = $address;
+                }
+            }
+        }
+
+        $orders_cache[$order['order_id']] = $order_data;
+        return $order_data;
+    }
+
+    private function getProduct($productId) {
+        $query = "SELECT * FROM `#__hikashop_product` WHERE `product_id` = $productId";
+        $result = $this->db->setQuery($query)->loadAssoc();
+        if ($result && $result['product_parent_id']) {
+            return $this->getProduct($result['product_parent_id']);
+        }
+        return $result;
+    }
+
+    public function updateOrderInvitesSend($order, $error) {
+        $now = time();
+        $this->db->setQuery("
+                INSERT INTO `#__webwinkelkeur_hikashop_order` SET
+                    `hikashop_order_id` = " . (int) $order['order_id'] . ",
+                    `success` = " . ($error ? '0' : '1') . ",
+                    `tries` = 1,
+                    `time` = " . $now . "
+                ON DUPLICATE KEY UPDATE
+                    `success` = IF(`success` = 1, 1, " . ($error ? '0' : '1') . "),
+                    `tries` = `tries` + 1,
+                    `time` = " . $now . "
+            ")->execute();
+    }
+
+}
--- extension/plg_sys_webwinkelkeur/webwinkelkeur.php
+++ extension/plg_sys_webwinkelkeur/webwinkelkeur.php
@@ -8,6 +8,7 @@ defined('_JEXEC') or die('Restricted access');
 
 require_once dirname(__FILE__) . '/WebwinkelKeurShopPlatform.php';
 require_once dirname(__FILE__) . '/WebwinkelKeurVirtuemartPlatform.php';
+require_once dirname(__FILE__) . '/WebwinkelKeurHikaShopPlatform.php';
 
 class PlgSystemWebwinkelKeur extends JPlugin {
     private $config;
@@ -22,7 +23,7 @@ class PlgSystemWebwinkelKeur extends JPlugin {
         $app = JFactory::getApplication();
         $db = JFactory::getDBO();
         if(!$app->isSite()) {
-            $this->sendHikashopInvites();
+            $this->sendPlatformInvites(new WebwinkelKeurHikaShopPlatform($db));
             $this->sendPlatformInvites(new WebwinkelKeurVirtuemartPlatform($db));
         }
     }
@@ -61,126 +62,12 @@ class PlgSystemWebwinkelKeur extends JPlugin {
         JFactory::getDocument()->addCustomTag($script);
     }
 
-    /*
-    * Hikashop functions
-    */
-    function plgHikashopName(&$subject, $config){
-        parent::__construct($subject, $config);
-    }
-
-    function sendHikashopInvites() {
-        $app = JFactory::getApplication();
-        $db = JFactory::getDBO();
-        $config = $this->getConfig();
-
-        // hikashop enabled?
-        $db->setQuery("SELECT enabled FROM #__extensions WHERE element = 'com_hikashop'");
-        $is_enabled = $db->loadResult();
-        if (!$is_enabled) {
-            return;
-        }
-
-        // invites enabled?
-        if(empty($config['invite'])
-           || empty($config['wwk_shop_id'])
-           || empty($config['wwk_api_key'])
-        )
-            return;
-
-        // find orders
-        $db->setQuery("
-            SELECT
-                ho.order_id,
-                ho.order_number,
-                hu.user_email,
-                CONCAT(ha.address_firstname, ' ', ha.address_lastname) as customername
-            FROM `#__hikashop_order` ho
-            INNER JOIN `#__hikashop_user` hu ON
-                ho.order_user_id = hu.user_id
-            LEFT JOIN `#__hikashop_address` ha ON
-                ho.order_billing_address_id = ha.address_id
-            LEFT JOIN `#__hikashop_zone` hz ON
-                ha.address_country = hz.zone_namekey
-            LEFT JOIN `#__webwinkelkeur_hikashop_order` who ON
-                who.hikashop_order_id = ho.order_id
-            WHERE
-                (
-                    who.hikashop_order_id IS NULL
-                    OR (
-                        who.success = 0
-                        AND who.tries <= 5
-                        AND who.time < " . (time() - 1800) . "
-                    )
-                )
-                AND hu.user_email LIKE '%@%'
-                AND ho.order_status = 'shipped'
-        ");
-        $orders = $db->loadAssocList();
-        if(!$orders)
-            return;
-
-        // process
-        require_once dirname(__FILE__) . '/api.php';
-        foreach($orders as $order) {
-            $api = new WebwinkelKeurAPI($config['wwk_shop_id'], $config['wwk_api_key']);
-            $error = null;
-            $url = null;
-            $data = array(
-                'order'     => $order['order_number'],
-                'email'     => $order['user_email'],
-                'delay'     => @$config['invite_delay'],
-                'language'  => null,
-                'client'    => 'hikashop',
-                'customer_name' => $order['customername']
-            );
-            if (@$config['invite'] == 2) {
-                $data['max_invitations_per_email'] = 1;
-            }
-
-            try {
-                $api->invite($data);
-            } catch(WebwinkelKeurAPIAlreadySentError $e) {
-            } catch(WebwinkelKeurAPIError $e) {
-                $error = $e->getMessage();
-                $url = $e->getURL();
-            }
-
-            $now = time();
-
-            $db->setQuery("
-                INSERT INTO `#__webwinkelkeur_hikashop_order` SET
-                    `hikashop_order_id` = " . (int) $order['order_id'] . ",
-                    `success` = " . ($error ? '0' : '1') . ",
-                    `tries` = 1,
-                    `time` = " . $now . "
-                ON DUPLICATE KEY UPDATE
-                    `success` = IF(`success` = 1, 1, " . ($error ? '0' : '1') . "),
-                    `tries` = `tries` + 1,
-                    `time` = " . $now . "
-            ");
-            $db->query();
-
-            if($error) {
-                $db->setQuery("
-                    INSERT INTO `#__webwinkelkeur_invite_error` SET
-                        `url` = " . $db->quote($url) . ",
-                        `response` = " . $db->quote($error) . ",
-                        `time` = " . $now . ",
-                        `reported` = 0
-                ");
-                $db->query();
-                $app->enqueueMessage("De WebwinkelKeur uitnodiging voor order {$order['order_number']} kon niet worden verstuurd. -- $error", 'error');
-            }
-        }
-    }
-
-
     private function sendPlatformInvites(WebwinkelKeurShopPlatform $platform) {
         $app = JFactory::getApplication();
         $db = JFactory::getDBO();
         $config = $this->getConfig();
 
-        // virtuemart enabled?
+        // platform enabled?
         list ($is_enabled, $platform_manifest) = $this->getExtensionInfo($platform->getExtensionName(), $db);
         if (!$is_enabled) {
             return;
@@ -228,6 +115,8 @@ class PlgSystemWebwinkelKeur extends JPlugin {
             if (empty ($config['limit_order_data']) || !$config['limit_order_data']) {
                 $data['order_data'] = json_encode($platform->getOrderData($order));
             }
+
+
             try {
                 $api->invite($data);
             } catch(WebwinkelKeurAPIAlreadySentError $e) {
